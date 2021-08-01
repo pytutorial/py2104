7def27cdf8f9758a5b82ef5b93ca48fd462ce710
Tung Duong <duongthanhtungvn01@gmail.com>
2021-08-01 20:16:22 +0700

    Update

--- Day20/project1/project1/urls.py
+++ Day20/project1/project1/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path 
+from views import index, page2
 urlpatterns = [
+    path('', index),
+    path('page-2', page2),
     path('admin/', admin.site.urls),
 ]
