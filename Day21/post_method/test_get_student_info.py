f3a8980542e590088b19801cc61fb7a598063b57
Tung Duong <duongthanhtungvn01@gmail.com>
2021-08-04 20:12:13 +0700

    Update

---
+++ Day21/post_method/test_get_student_info.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+text = requests.get('http://127.0.0.1:8000/get-student-info?number=1001').text
+print(text)
+student = json.loads(text)
+print(f'Tên: {student["name"]}, ngày sinh: {student["birthDate"]}')
