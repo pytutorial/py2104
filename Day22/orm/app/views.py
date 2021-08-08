2606c8cfa4bb5563b1f4da3019f5c9baf1ab29a6
Tung Duong <duongthanhtungvn01@gmail.com>
2021-08-08 20:41:45 +0700

    Update

--- Day22/orm/app/views.py
+++ Day22/orm/app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import HttpResponse
 from .models import *
+import json
 
 # Create your views here.
 def get_product_by_code(request, code):
     product = Product.objects.get(code=code)
-    return HttpResponse(product.name)
+    data = {
+        'id': product.id,
+        'name': product.name,
+        'code': product.code,
+        'description': product.description,
+        'price': product.price
+    }
+    return HttpResponse(json.dumps(data))
