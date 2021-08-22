7b13209018bb80bed68d76fcee8b4ca8f5454539
Tung Duong <duongthanhtungvn01@gmail.com>
2021-08-22 19:19:13 +0700

    Update

--- Day26/viewclass_viewset/app/models.py
+++ Day26/viewclass_viewset/app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    phone = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    code = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500, blank=True)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    qty = models.IntegerField()
+    priceUnit = models.IntegerField()
+    orderDate = models.DateTimeField()
