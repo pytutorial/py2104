b6484599e7bac1389cbc2701b2eafd059a2d5123
Tung Duong <duongthanhtungvn01@gmail.com>
2021-09-08 21:20:25 +0700

    Update

---
+++ Day30/vd6.js
@@ -0,0 +1,9 @@
+// Cho ho, ten dem, ten cua 1 nguoi
+// In ra ho ten day du
+var ho = 'Nguyen';
+var ten_dem = 'Van';
+var ten = 'An';
+
+var ho_ten = ho + ' ' + ten_dem + ' ' + ten;
+ho_ten = `${ho} ${ten_dem} ${ten}`;
+console.log(ho_ten)
---
+++ Day30/vd6.py
@@ -0,0 +1,9 @@
+# Cho ho, ten dem, ten cua 1 nguoi
+# In ra ho ten day du
+ho = 'Nguyen'
+ten_dem = 'Van'
+ten = 'An'
+
+ho_ten = ho + ' ' + ten_dem + ' ' + ten
+ho_ten = f'{ho}-{ten_dem}-{ten}'
+print(ho_ten)
