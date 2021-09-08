cef015dfac2f190d3662fe027c8c76128940257d
Tung Duong <duongthanhtungvn01@gmail.com>
2021-09-08 20:53:26 +0700

    Update

---
+++ Day30/vd2.js
@@ -0,0 +1,9 @@
+// cho 2 so --> in ra so lon hon
+var a = 10;
+var b = 5;
+
+if(a < b){
+    console.log(b);
+}else {
+    console.log(a);
+}
---
+++ Day30/vd2.py
@@ -0,0 +1,8 @@
+# cho 2 so --> in ra so lon hon
+a = 10
+b = 5
+
+if a < b:
+    print(b)
+else:
+    print(a)
