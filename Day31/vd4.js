dbe45c06e79c401249972e7078503e921a804d85
Tung Duong <duongthanhtungvn01@gmail.com>
2021-09-12 19:38:32 +0700

    Update

---
+++ Day31/vd4.js
@@ -0,0 +1,19 @@
+// Cho bang order san pham
+// Tinh tong doanh so
+var orderList = [
+    {
+        productName: 'IPhone X',
+        qty: 1,
+        priceUnit: 10.5
+    },
+    {
+        productName: 'Nokia 6.1',
+        qty: 2,
+        priceUnit: 4.5
+    },
+    {
+        productName: 'IPhone 12',
+        qty: 1,
+        priceUnit: 22.5
+    }
+]
