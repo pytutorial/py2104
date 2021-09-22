8a0b173110c57af6b433467e7653636035c44c75
Tung Duong <duongthanhtungvn01@gmail.com>
2021-09-22 19:14:53 +0700

    Update

--- Day34/backend/app/models.py
+++ Day34/backend/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    phone = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
