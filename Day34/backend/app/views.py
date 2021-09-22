f0d501596ec6fa5a5872d89f87445affc1df0090
Tung Duong <duongthanhtungvn01@gmail.com>
2021-09-22 19:39:33 +0700

    Update

--- Day34/backend/app/views.py
+++ Day34/backend/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Customer
 
 # Create your views here.
 def index(request):
@@ -6,4 +7,6 @@ def index(request):
 
 #127.0.0.1:8000/list-customer
 def list_customer(request):
-    return render(request, 'list_customer.html')
+    customer_list = Customer.objects.all()
+    context = {'customer_list': customer_list}
+    return render(request, 'list_customer.html', context)
