3049c87853fbcfed861ccd90adedb8783e866cd4
Tung Duong <duongthanhtungvn01@gmail.com>
2021-09-29 18:54:57 +0700

    Update

---
+++ Day36/backend/app/views_api.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.serializers import ModelSerializer
+
+from .models import Customer
+
+class CustomerSerializer(ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+@api_view(['GET'])
+def get_customer_list(request):
+    customer_list = Customer.objects.all()
+    data = CustomerSerializer(customer_list, many=True).data
+    return Response(data)
