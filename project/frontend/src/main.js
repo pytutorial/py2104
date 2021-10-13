4271fd0461a573a39ff6181258c0ffe955cfe420
Tung Duong <duongthanhtungvn01@gmail.com>
2021-10-13 20:15:57 +0700

    Update

--- project/frontend/src/main.js
+++ project/frontend/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue } from 'bootstrap-vue';
+import router from './router';
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue);
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
