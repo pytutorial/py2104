234f8e1e1fbc621532405b5eb47cbd80e7601da0
Tung Duong <duongthanhtungvn01@gmail.com>
2021-10-13 20:37:35 +0700

    Update

--- project/frontend/src/pages/ViewProduct.vue
+++ project/frontend/src/pages/ViewProduct.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        Xem sản phẩm
+  <div id="app" class="container mt-5">
+    <div class="row">
+      <div class="col-6">
+        <img width="100%" :src='product.image' />
+      </div>
+      <div class="col-6 pt-5">
+        <h4>{{product.name}}</h4>
+        <table class="table">
+          <tbody>
+            <tr>
+              <th>Hãng sản xuất:</th>
+              <td>{{product.category_name}}</td>
+            </tr>
+            <tr>
+              <th>Giá bán:</th>
+              <td>{{product.price}} ₫</td>
+            </tr>
+          </tbody>
+        </table>
+        <a href='index.html' class="btn btn-secondary">Quay lại</a>
+        <a :href='`order_product.html?id=${product.id}`' class="btn btn-primary">Mua hàng</a>
+      </div>
     </div>
+  </div>
 </template>
