befe6dd144f450bb72ea238825de08fbeedf359f
Tung Duong <duongthanhtungvn01@gmail.com>
2021-10-13 20:32:41 +0700

    Update

--- project/frontend/src/router.js
+++ project/frontend/src/router.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/pages/Home'
+import ViewProduct from '@/pages/ViewProduct'
 Vue.use(VueRouter);
 
 const routes = [
     {path: '/', component: Home, meta:{page:1}},
+    {path: '/view-product/:id', component: ViewProduct, meta:{page:1}},
 ]
 
 const router = new VueRouter({routes})
