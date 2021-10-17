ee4e8f4d327204e6b49c7ae6178c1b8b4a484d44
Tung Duong <duongthanhtungvn01@gmail.com>
2021-10-17 19:36:21 +0700

    Update

--- Day40/authorization/app/views.py
+++ Day40/authorization/app/views.py
@@ -1,6 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
 @api_view(['GET'])
+@permission_classes((IsAuthenticated,))
 def hello(request):
     return Response({'message': 'Hello'})
