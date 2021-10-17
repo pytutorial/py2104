bfa773520e421525e7ac3ec88156b6059dab9f2f
Tung Duong <duongthanhtungvn01@gmail.com>
2021-10-17 20:35:52 +0700

    Update

--- project/backend/backend/settings.py
+++ project/backend/backend/settings.py
@@ -133,3 +133,14 @@ import os
 STATICFILES_DIRS  = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+from datetime import timedelta
+SIMPLE_JWT = {
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
+}
+
+REST_FRAMEWORK = {
+  'DEFAULT_AUTHENTICATION_CLASSES': [       
+      'rest_framework_simplejwt.authentication.JWTAuthentication',
+  ],
+}
