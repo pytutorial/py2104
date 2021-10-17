86a11fc8584f4246bd44c15f89c202f04e868ce8
Tung Duong <duongthanhtungvn01@gmail.com>
2021-10-17 21:14:25 +0700

    Update

---
+++ project/frontend/src/AppStaff.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      Trang dành riêng cho nhân viên
+  </div>
+</template>
+<script>
+export default{
+  mounted: function() {
+    if(!localStorage.getItem('token')){
+      this.$router.push('/login');
+    }
+  }
+}
+</script>
