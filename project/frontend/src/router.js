6f67b279034eb1fa3f2f2363b1bfde68f627131f
Tung Duong <duongthanhtungvn01@gmail.com>
2021-10-17 20:47:33 +0700

    Update

--- project/frontend/src/router.js
+++ project/frontend/src/router.js
@@ -4,6 +4,7 @@ import Home from '@/pages/Home'
 import ViewProduct from '@/pages/ViewProduct'
 import OrderProduct from '@/pages/OrderProduct'
 import ThankYou from '@/pages/ThankYou'
+import Login from '@/pages/Login'
 Vue.use(VueRouter);
 
 const routes = [
@@ -11,6 +12,7 @@ const routes = [
     {path: '/view-product/:id', component: ViewProduct, meta:{page:1}},
     {path: '/order-product/:id', component: OrderProduct, meta:{page:1}},
     {path: '/thank-you', component: ThankYou, meta:{page:1}},
+    {path: '/login', component: Login},
 ]
 
 const router = new VueRouter({routes})
