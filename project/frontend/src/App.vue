a88fde04d66c78bbf2718da8fd8f2852054be003
Tung Duong <duongthanhtungvn01@gmail.com>
2021-10-17 21:19:58 +0700

    Update

--- project/frontend/src/App.vue
+++ project/frontend/src/App.vue
@@ -1,3 +1,14 @@
 <template>
-  <router-view></router-view> 
+  <div>
+    <app-staff v-if="$route.path.startsWith('/staff')"></app-staff>
+    <router-view v-else></router-view>
+  </div>
 </template>
+<script>
+import AppStaff from './AppStaff.vue';
+export default {
+  components:{
+    AppStaff
+  }
+}
+</script>
